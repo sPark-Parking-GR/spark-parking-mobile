30aed68f567523d97c92eaaf826429190c0978e4
Konstantinos Thomasiadis <k.thomasiadis@ysoft.gr>
2026-09-03 01:40:52 +0300

    expo version upgrade

--- app.config.ts
+++ app.config.ts
@@ -6,6 +6,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...(config as ExpoConfig),
   plugins: [
     ...(config.plugins ?? []),
+    "expo-font",
     [
       'react-native-maps',
       {
